2fdb896e148f940de1ef036816e1a832da857186
prudhvi <prudhviram2002@gmail.com>
2025-11-17 12:06:24 +0530

    initial commit

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,69 @@
+import {Component} from 'react'
+
+import NxtWatchAppContext from '../../Context/NxtWatchAppContext'
+
+import Navbar from '../Navbar'
+import Sidebar from '../Sidebar'
+import LoaderCard from '../LoaderCard'
+
+import {
+  NotFoundDisplayCard,
+  NotFoundBgContainer,
+  NotFoundImage,
+  NotFoundHeading,
+  NotFoundDescription,
+  NotFoundButton,
+} from './styledComponent'
+
+class NotFoundRoute extends Component {
+  state = {isLoaderLoading: true}
+
+  navigateToHome = () => {
+    const {history} = this.props
+    history.replace('/')
+  }
+
+  render() {
+    const {isLoaderLoading} = this.state
+    return (
+      <NxtWatchAppContext.Consumer>
+        {value => {
+          const {isDarkModeEnabled} = value
+          return (
+            <>
+              <Navbar />
+              <NotFoundDisplayCard isDarkModeEnabled={isDarkModeEnabled}>
+                <Sidebar />
+                {isLoaderLoading ? (
+                  <LoaderCard />
+                ) : (
+                  <NotFoundBgContainer isDarkModeEnabled={isDarkModeEnabled}>
+                    <NotFoundImage
+                      src={
+                        isDarkModeEnabled
+                          ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-not-found-dark-theme-img.png'
+                          : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-not-found-light-theme-img.png'
+                      }
+                      alt="not found"
+                    />
+                    <NotFoundHeading isDarkModeEnabled={isDarkModeEnabled}>
+                      Page Not Found
+                    </NotFoundHeading>
+                    <NotFoundDescription isDarkModeEnabled={isDarkModeEnabled}>
+                      We are sorry, the page you requested could not be found.
+                    </NotFoundDescription>
+                    <NotFoundButton type="button" onClick={this.navigateToHome}>
+                      Navigate To Home
+                    </NotFoundButton>
+                  </NotFoundBgContainer>
+                )}
+              </NotFoundDisplayCard>
+            </>
+          )
+        }}
+      </NxtWatchAppContext.Consumer>
+    )
+  }
+}
+
+export default NotFoundRoute
---
+++ src/setupTests.js
@@ -0,0 +1,3 @@
+// src/setupTests.js
+import '@testing-library/jest-dom'
+import {configure} from '@testing-library/react'
